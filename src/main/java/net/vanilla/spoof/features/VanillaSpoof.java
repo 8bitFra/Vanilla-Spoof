f67dbb682c31e4b061696b325424923635f45238
8bitFra <sonosolofra@gmail.com>
2022-10-26 23:29:58 +0200

    1.19

--- src/main/java/net/vanilla/spoof/features/VanillaSpoof.java
+++ src/main/java/net/vanilla/spoof/features/VanillaSpoof.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2021 Wurst-Imperium and contributors.
+ * Copyright (c) 2014-2022 Wurst-Imperium and contributors.
  *
  * This source code is subject to the terms of the GNU General Public
  * License, version 3. If a copy of the GPL was not distributed with this
@@ -8,14 +8,16 @@
 package net.vanilla.spoof.features;
 
 import io.netty.buffer.Unpooled;
-import net.fabricmc.fabric.mixin.networking.accessor.CustomPayloadC2SPacketAccessor;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.c2s.play.CustomPayloadC2SPacket;
-import net.vanilla.spoof.Main;
 import net.vanilla.spoof.events.ConnectionPacketOutputListener;
+import net.vanilla.spoof.Main;
+import net.vanilla.spoof.mixin.CustomPayloadC2SPacketAccessor;
+
 
 public final class VanillaSpoof implements ConnectionPacketOutputListener
 {
+
 	public VanillaSpoof()
 	{
 		Main.eventManager.add(ConnectionPacketOutputListener.class, this);
@@ -23,11 +25,13 @@ public final class VanillaSpoof implements ConnectionPacketOutputListener
 	
 	@Override
 	public void onSentConnectionPacket(ConnectionPacketOutputEvent event)
-	{	
-		if(!(event.getPacket() instanceof CustomPayloadC2SPacket))
+	{
+		
+		if(!(event.getPacket() instanceof CustomPayloadC2SPacketAccessor))
 			return;
 		
 		CustomPayloadC2SPacketAccessor packet = (CustomPayloadC2SPacketAccessor)event.getPacket();
+		
 		if(packet.getChannel().getNamespace().equals("minecraft") && packet.getChannel().getPath().equals("register"))
 			event.cancel();
 		
--- src/main/java/net/vanilla/spoof/mixin/ClientConnectionMixin.java
+++ src/main/java/net/vanilla/spoof/mixin/ClientConnectionMixin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2021 Wurst-Imperium and contributors.
+ * Copyright (c) 2014-2022 Wurst-Imperium and contributors.
  *
  * This source code is subject to the terms of the GNU General Public
  * License, version 3. If a copy of the GPL was not distributed with this
@@ -7,6 +7,9 @@
  */
 package net.vanilla.spoof.mixin;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,10 +18,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.GenericFutureListener;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.Packet;
+import net.minecraft.network.PacketCallbacks;
 import net.vanilla.spoof.event.EventManager;
 import net.vanilla.spoof.events.ConnectionPacketOutputListener.ConnectionPacketOutputEvent;
 import net.vanilla.spoof.events.PacketInputListener.PacketInputEvent;
@@ -27,7 +29,7 @@ import net.vanilla.spoof.events.PacketInputListener.PacketInputEvent;
 public abstract class ClientConnectionMixin
 	extends SimpleChannelInboundHandler<Packet<?>>
 {
-	private ConnectionPacketOutputEvent event;
+	private ConcurrentLinkedQueue<ConnectionPacketOutputEvent> events = new ConcurrentLinkedQueue<>();
 	
 	@Inject(at = {@At(value = "INVOKE",
 		target = "Lnet/minecraft/network/ClientConnection;handlePacket(Lnet/minecraft/network/Packet;Lnet/minecraft/network/listener/PacketListener;)V",
@@ -46,26 +48,39 @@ public abstract class ClientConnectionMixin
 	}
 	
 	@ModifyVariable(
-		method = "send(Lnet/minecraft/network/Packet;Lio/netty/util/concurrent/GenericFutureListener;)V",
+		method = "send(Lnet/minecraft/network/Packet;Lnet/minecraft/network/PacketCallbacks;)V",
 		at = @At("HEAD"))
 	public Packet<?> onSendPacket(Packet<?> packet)
 	{
-		event = new ConnectionPacketOutputEvent(packet);
+		ConnectionPacketOutputEvent event = new ConnectionPacketOutputEvent(packet);
+		events.add(event);
 		EventManager.fire(event);
 		return event.getPacket();
 	}
 	
 	@Inject(at = {@At(value = "HEAD")},
 		method = {
-			"send(Lnet/minecraft/network/Packet;Lio/netty/util/concurrent/GenericFutureListener;)V"},
+			"send(Lnet/minecraft/network/Packet;Lnet/minecraft/network/PacketCallbacks;)V"},
 		cancellable = true)
 	private void onSendPacket(Packet<?> packet,
-		GenericFutureListener<? extends Future<? super Void>> callback,
-		CallbackInfo ci)
+		@Nullable PacketCallbacks callback, CallbackInfo ci)
 	{
-		if(event != null && event.isCancelled())
+		ConnectionPacketOutputEvent event = getEvent(packet);
+		if(event == null)
+			return;
+		
+		if(event.isCancelled())
 			ci.cancel();
 		
-		event = null;
+		events.remove(event);
+	}
+	
+	private ConnectionPacketOutputEvent getEvent(Packet<?> packet)
+	{
+		for(ConnectionPacketOutputEvent event : events)
+			if(event.getPacket() == packet)
+				return event;
+			
+		return null;
 	}
 }
